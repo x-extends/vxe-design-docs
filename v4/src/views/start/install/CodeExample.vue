7edbccabb62d12929e2c20b7e03c0b882d30b13b
xuliangzhan <xu_liangzhan@163.com>
2025-07-08 22:03:05 +0800

    update docs

--- v4/src/views/start/install/CodeExample.vue
+++ v4/src/views/start/install/CodeExample.vue
@@ -16,24 +16,28 @@
           <pre-code
             language="shell"
             :content="`
-            npm install ${uiCDNLib } ${ tableCDNLib }
+            npm install ${uiCDNLib } ${ tableCDNLib } ${ designCDNLib }
             # 或者
-            yarn add ${uiCDNLib } ${ tableCDNLib }
+            yarn add ${uiCDNLib } ${ tableCDNLib } ${ designCDNLib }
             # 或者
-            pnpm add ${uiCDNLib } ${ tableCDNLib }
+            pnpm add ${uiCDNLib } ${ tableCDNLib } ${ designCDNLib }
             `">
           </pre-code>
           <pre-code
             language="javascript"
             content="
-             // ...
+            // ...
             import VxeUIAll from 'vxe-pc-ui'
-            import 'vxe-pc-ui/lib/style.css'
+            import 'vxe-pc-ui/es/style.css'
+
             import VxeUITable from 'vxe-table'
-            import 'vxe-table/lib/style.css'
+            import 'vxe-table/es/style.css'
+
+            import VxeUIDesign from 'vxe-design'
+            import 'vxe-design/lib/style.css'
             // ...
 
-            createApp(App).use(VxeUIAll).use(VxeUITable).mount('#app')
+            createApp(App).use(VxeUIAll).use(VxeUITable).use(VxeUIDesign).mount('#app')
             // ...">
           </pre-code>
         </pre>
@@ -42,8 +46,8 @@
 
     <CodeLight>
       <template #tip>
-        <vxe-tip status="primary" title="框架模板示例">
-          <div>极致精简、流畅的框架模板 <vxe-link href="https://github.com/x-extends/vxe-admin-template" target="_blank"><vxe-icon name="github-fill"></vxe-icon>vxe-admin-template</vxe-link>、<vxe-link href="https://gitee.com/x-extends/vxe-admin-template" target="_blank"><vxe-icon name="gitee-fill" status="error"></vxe-icon>vxe-admin-template</vxe-link></div>
+        <vxe-tip status="primary" title="设计器模板示例">
+          <div>支持任意扩展，自定义控件的可视化设计器模板 <vxe-link href="https://github.com/x-extends/vxe-design-template" target="_blank"><vxe-icon name="github-fill"></vxe-icon>vxe-design-template</vxe-link>、<vxe-link href="https://gitee.com/x-extends/vxe-design-template" target="_blank"><vxe-icon name="gitee-fill" status="error"></vxe-icon>vxe-design-template</vxe-link>，<vxe-link href="https://vxeui.com/design-template/" target="_blank" status="primary">点击在线预览</vxe-link></div>
         </vxe-tip>
       </template>
 
@@ -52,16 +56,16 @@
           <pre-code
             language="shell"
             :content="`
-            git clone https://gitee.com/x-extends/vxe-admin-template.git
+            git clone https://gitee.com/x-extends/vxe-design-template.git
             # 或者
-            git clone https://github.com/x-extends/vxe-admin-template.git
+            git clone https://github.com/x-extends/vxe-design-template.git
             `">
           </pre-code>
           <pre-code
             language="shell"
             content="
             # 切换目录
-            cd vxe-admin-template
+            cd vxe-design-template
 
             # 更新依赖
             npm run update
@@ -89,4 +93,5 @@ import { useAppStore } from '@/store/app'
 const appStore = useAppStore()
 const uiCDNLib = computed(() => appStore.uiCDNLib)
 const tableCDNLib = computed(() => appStore.tableCDNLib)
+const designCDNLib = computed(() => appStore.designCDNLib)
 </script>
