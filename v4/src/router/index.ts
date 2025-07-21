2b41b77a1ca7f452db84da61cc2789bfa9f55fb2
xuliangzhan <xu_liangzhan@163.com>
2025-07-21 18:50:57 +0800

    update docs

--- v4/src/router/index.ts
+++ v4/src/router/index.ts
@@ -504,6 +504,16 @@ const routes: Array<RouteRecordRaw> = [
                     path: 'myTextareaWidget',
                     name: 'GlobalRendererFormDesignWidgetMyTextareaWidget',
                     component: () => import('@/views/global/renderer/form-design/widget/myTextareaWidget/CodeExample.vue')
+                  },
+                  {
+                    path: 'mySelectWidget',
+                    name: 'GlobalRendererFormDesignWidgetMySelectWidget',
+                    component: () => import('@/views/global/renderer/form-design/widget/mySelectWidget/CodeExample.vue')
+                  },
+                  {
+                    path: 'myDatePickerWidget',
+                    name: 'GlobalRendererFormDesignWidgetMyDatePickerWidget',
+                    component: () => import('@/views/global/renderer/form-design/widget/myDatePickerWidget/CodeExample.vue')
                   }
                 ]
               }
