63a3859cac16d916b98194316d90d7cdf60f1f76
xuliangzhan <xu_liangzhan@163.com>
2025-07-25 15:16:54 +0800

    update docs

--- v3/src/common/nav-config.ts
+++ v3/src/common/nav-config.ts
@@ -1,5 +1,5 @@
 import XEUtils from 'xe-utils'
-import { NavVO } from './nav'
+import { NavVO, otherUrl } from './nav'
 import { designNavConfig } from './design'
 
 XEUtils.eachTree(designNavConfig.children.slice(0, 1), (item, index, items, path, parent, nodes) => {
@@ -30,7 +30,8 @@ export const navConfigList: NavVO[] = [
           { i18nKey: 'app.aside.menu.globalInstall', routerLink: { name: 'StartTableInstall' } },
           { i18nKey: 'app.aside.menu.lazyUseGlobal', routerLink: { name: 'StartTableUseGlobal' } },
           { i18nKey: 'app.aside.menu.lazyUseImport', routerLink: { name: 'StartTableUseImport' } },
-          { i18nKey: 'app.aside.menu.useCDN', routerLink: { name: 'StartTableCDN' } }
+          { i18nKey: 'app.aside.menu.useCDN', routerLink: { name: 'StartTableCDN' } },
+          { title: '使用第三方 UI 库', isPlugin: true, linkUrl: `${otherUrl}/#/plugin-render-element/grid/edit/el-input` }
         ]
       },
       // {
