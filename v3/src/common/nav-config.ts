ac82e0ff3d05aa970a154e768b46842a7a68a917
xuliangzhan <xu_liangzhan@163.com>
2025-07-25 14:49:00 +0800

    update docs

--- v3/src/common/nav-config.ts
+++ v3/src/common/nav-config.ts
@@ -1,85 +1,12 @@
-export interface NavVO {
-  title?: string
-  name?: string
-  i18nKey?: string
-  isAllAPI?: boolean
-  isSelfAPI?: boolean
-  isExpand?: boolean
-  isNew?: boolean
-  isUnpublished?: boolean
-  isEnterprise?: boolean
-  isPlugin?: boolean
-  isTemplate?: boolean
-  routerLink?: {
-    name: string
-    params?: Record<string, string | number>
-    query?: Record<string, string | number>
+import XEUtils from 'xe-utils'
+import { NavVO } from './nav'
+import { designNavConfig } from './design'
+
+XEUtils.eachTree(designNavConfig.children.slice(0, 1), (item, index, items, path, parent, nodes) => {
+  if (nodes.length <= 2) {
+    item.isExpand = true
   }
-  linkUrl?: string
-  linkTarget?: '_self' | '_blank'
-  linkStatus?: 'warning' | 'success' | 'error'
-  isBack?: boolean
-  keywords?: string
-  children?: NavVO[]
-}
-
-export const otherUrl = `${process.env.VUE_APP_SITE_PLUGIN_URL}/other${process.env.VUE_APP_VXE_VERSION}`
-export const tablePluginDocsUrl = `${process.env.VUE_APP_SITE_PLUGIN_URL}/pluginDocs/table${process.env.VUE_APP_VXE_VERSION}`
-
-const designNavConfig: NavVO & { children: NavVO[] } = {
-  i18nKey: 'app.aside.menu.designTitle',
-  children: [
-    {
-      i18nKey: 'app.aside.menu.formDesignTitle',
-      children: [
-        { title: 'API', isSelfAPI: true, routerLink: { name: 'DocsApi', params: { name: 'form-design' } } }
-        // {
-        //   i18nKey: 'app.aside.menu.formDesignUse',
-        //   children: [
-        //     { title: '基础功能', routerLink: { name: 'ComponentFormDesignBase' } },
-        //     { title: '自定义控件分组', routerLink: { name: 'ComponentFormDesignCustomGroup' } },
-        //     { title: '移动端设计器', routerLink: { name: 'ComponentFormDesignMobile' } },
-        //     { title: 'PC 端和移动端设计器', routerLink: { name: 'ComponentFormDesignPcAndMobile' } },
-        //     { title: '渲染视图', routerLink: { name: 'ComponentFormDesignRenderView' } },
-        //     { title: '集成第三方扩展插件', isPlugin: true, linkUrl: `${otherUrl}/#/plugin-render-element/form-design/renderView` }
-        //   ]
-        // },
-        // {
-        //   i18nKey: 'app.aside.menu.formDesignGlobalRenderer',
-        //   children: [
-        //     { title: 'API', routerLink: { name: 'GlobalRendererFormDesignAPI' } },
-        //     {
-        //       title: '控件',
-        //       children: [
-        //         { title: '实现一个单行输入控件', routerLink: { name: 'GlobalRendererFormDesignWidgetMyInputWidget' } },
-        //         { title: '实现一个多行输入控件', routerLink: { name: 'GlobalRendererFormDesignWidgetMyTextareaWidget' } }
-        //       ]
-        //     }
-        //   ]
-        // }
-      ]
-    },
-    {
-      i18nKey: 'app.aside.menu.listDesignTitle',
-      children: [
-        { title: 'API', isSelfAPI: true, routerLink: { name: 'DocsApi', params: { name: 'list-design' } } }
-        // {
-        //   i18nKey: 'app.aside.menu.listDesignUse',
-        //   children: [
-        //     { title: '基础功能', routerLink: { name: 'ComponentListDesignBase' } },
-        //     { title: '移动端设计器', routerLink: { name: 'ComponentListDesignMobile' } },
-        //     { title: 'PC 端和移动端设计器', routerLink: { name: 'ComponentListDesignPcAndMobile' } },
-        //     { title: '集成表单设计器', routerLink: { name: 'ComponentListDesignFormAndList' } }
-        //   ]
-        // }
-      ]
-    }
-    // { title: 'FlowDesign 流程设计器' },
-    // { title: 'PrintDesign 打印设计器' },
-    // { title: '可视化设计器模板', isTemplate: true, linkUrl: 'https://vxeui.com/design-template/' },
-    // { title: '零代码平台模板', isEnterprise: true, linkUrl: 'https://vxeui.com/zero-code/' }
-  ]
-}
+})
 
 export const navConfigList: NavVO[] = [
   {
