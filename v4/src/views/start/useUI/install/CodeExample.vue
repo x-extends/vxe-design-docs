6d701a48e49fa0400800a718a26d1c7d010e8c67
xuliangzhan <xu_liangzhan@163.com>
2025-12-14 17:59:32 +0800

    update docs

--- v4/src/views/start/useUI/install/CodeExample.vue
+++ v4/src/views/start/useUI/install/CodeExample.vue
@@ -17,11 +17,11 @@
           <pre-code
             language="shell"
             :content="`
-            npm install ${uiCDNLib } ${ designCDNLib } ${ designCDNLib }
+            npm install ${utilCDNLib} ${uiCDNLib } ${ designCDNLib } ${ designCDNLib }
             # 或者
-            yarn add ${uiCDNLib } ${ designCDNLib } ${ designCDNLib }
+            yarn add ${utilCDNLib} ${uiCDNLib } ${ designCDNLib } ${ designCDNLib }
             # 或者
-            pnpm add ${uiCDNLib } ${ tableCDNLib } ${ designCDNLib }
+            pnpm add ${utilCDNLib} ${uiCDNLib } ${ tableCDNLib } ${ designCDNLib }
             `">
           </pre-code>
           <pre-code
