779ff3a70d60b2007d830d16c47898dce86360e3
xuliangzhan <xu_liangzhan@163.com>
2025-12-20 11:37:29 +0800

    update docs

--- v3/src/common/nav-config.ts
+++ v3/src/common/nav-config.ts
@@ -20,7 +20,7 @@ export const navConfigList: NavVO[] = [
           { i18nKey: 'app.aside.menu.globalInstall', routerLink: { name: 'StartUIInstall' } },
           // { i18nKey: 'app.aside.menu.lazyUseGlobal', routerLink: { name: 'StartUIUseGlobal' } },
           // { i18nKey: 'app.aside.menu.lazyUseImport', routerLink: { name: 'StartUIUseImport' } },
-          { i18nKey: 'app.aside.menu.useGlobalCDN', routerLink: { name: 'StartUICDN' } },
+          { i18nKey: 'app.aside.menu.useGlobalCDN', routerLink: { name: 'StartUICDN' } }
           // { title: '后台管理系统模板', isTemplate: true, linkUrl: 'https://vxeui.com/admin-template/' }
         ]
       },
@@ -30,7 +30,7 @@ export const navConfigList: NavVO[] = [
           { i18nKey: 'app.aside.menu.globalInstall', routerLink: { name: 'StartTableInstall' } },
           // { i18nKey: 'app.aside.menu.lazyUseGlobal', routerLink: { name: 'StartTableUseGlobal' } },
           // { i18nKey: 'app.aside.menu.lazyUseImport', routerLink: { name: 'StartTableUseImport' } },
-          { i18nKey: 'app.aside.menu.useGlobalCDN', routerLink: { name: 'StartTableCDN' } },
+          { i18nKey: 'app.aside.menu.useGlobalCDN', routerLink: { name: 'StartTableCDN' } }
           // { title: '使用第三方 UI 库', isPlugin: true, linkUrl: `${otherUrl}/#/plugin-render-element/grid/edit/el-input` }
         ]
       },
