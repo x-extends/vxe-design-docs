478377af10c9114c7ff559be896faef2b8d959d1
xuliangzhan <xu_liangzhan@163.com>
2026-01-15 20:38:19 +0800

    update docs

---
+++ v4/src/views/form-design/base/basics/Demo1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="row-wrapper">
+      <vxe-button status="primary" @click="clickEvent">获取json</vxe-button>
+      <vxe-form-design ref="formDesignRef" :widgets="formDesignWidgets" :height="800" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { VxeFormDesignPropTypes, VxeFormDesignInstance } from 'vxe-design'
+
+const formDesignRef = ref<VxeFormDesignInstance>()
+
+const formDesignWidgets = ref<VxeFormDesignPropTypes.Widgets>([
+  {
+    group: 'base',
+    children: [
+      'VxeInput'
+    ]
+  }
+])
+
+const clickEvent = () => {
+  const $formDesign = formDesignRef.value
+  if ($formDesign) {
+    console.log(JSON.stringify($formDesign.getConfig()))
+  }
+}
+</script>
